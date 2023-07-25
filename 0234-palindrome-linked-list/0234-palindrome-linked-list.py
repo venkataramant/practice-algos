6e6b31cbf20d21030336caaf9c2c3779b95a25b6
V RAMAN T <venkataramant@gmail.com>
2023-07-25 15:12:41 -0700

    Time: 1473 ms (5.02%), Space: 90.3 MB (5.09%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -20,6 +20,62 @@ class Solution:
         middleIndex = 0
         currentNode = None
         
+        originalHead = head
+        while(fPointer != None and fPointer.next != None):
+            sPointer = sPointer.next
+            fPointer = fPointer.next.next
+            middleIndex += 1
+        totalSize = middleIndex * 2
+        if fPointer != None:
+            totalSize += 1
+            sPointer = sPointer.next
+            print("last Node exists", totalSize)
+            
+        print("Middle Node middleIndex", middleIndex, "totalSize", totalSize, "midValue", sPointer.val)
+        
+        isPalinDrome, _ = self.verifyPalindrome(originalHead, sPointer.val, sPointer.next)
+        return isPalinDrome
+
+    def verifyPalindrome(self, originalHead, val1, middlePointer, index=1):
+        if middlePointer!=None:
+            print("verifyPalindrome...", val1, index, middlePointer.val)
+        else:
+            print("verifyPalindrome...", val1, index, "None")
+        if middlePointer == None:
+            if originalHead.val == val1:
+                print("passed ",val1,index)
+                return True, originalHead.next
+            else:
+                print("not matched ", originalHead.val , val1)
+                return False, None
+        else:
+            lastCheck, newOrgHead = self.verifyPalindrome(originalHead, middlePointer.val, middlePointer.next, index + 1)
+            if lastCheck:
+                if newOrgHead.val == val1:
+                    return True, newOrgHead.next
+                else:
+                    
+                    print("not matched ", newOrgHead.val , val1, index, middlePointer.val, lastCheck, newOrgHead.val,"index ",index)
+                    return False, None
+            else:
+                return lastCheck, None
+        return False, None
+    def isPalindrome2(self, head: Optional[ListNode]) -> bool:
+        if head == None:
+            print("has zero nodes")
+            return False
+        if head.next == None:
+            print("has 1 node")
+            return True
+        if head.next.next == None:
+            print(" has 2 nodes")
+            return head.val == head.next.val
+        
+        sPointer = head
+        fPointer = head
+        middleIndex = 0
+        currentNode = None
+        
         newHead = None
         while(fPointer != None and fPointer.next != None):
             newNode = ListNode(sPointer.val)
