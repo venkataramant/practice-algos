4be698ba3937030f9dd6059a4063e00d689680a1
V RAMAN T <venkataramant@gmail.com>
2023-07-25 18:38:07 -0700

    Time: 65 ms (27.08%), Space: 17.1 MB (29.1%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -3,20 +3,101 @@ class Solution:
         if matrix == None:
             return False
         rowM = len(matrix)
-        rowN = None
+        columnN = None
         if rowM > 0:
-            rowN = len(matrix[0])
+            columnN = len(matrix[0])
         else:
             return False
         left = 0
-        right = rowM * rowN
-        # print("starting", right, rowM, rowN)
+        right = rowM
+        # print("determine the row")
+        rowId = None
         while(left + 1 < right):
             mid = (left + right) // 2
             
-            #midValue = self.getValue(matrix, mid, rowN)
+            midValue = matrix[mid][0]
+            if target == midValue:
+                print("found ..", (mid, 0), matrix[mid][0])
+                return True
+            elif target > midValue:
+                left = mid
+            else:
+                right = mid
+        print(left, right, rowM)
+        if left < rowM - 1:
+            # midM, midN = left // rowN, left % rowN
+            midValue = matrix[left][columnN - 1]
+            if target == midValue:
+                rowId = left
+                print("found ..", (rowId, columnN - 1), matrix[rowId][columnN - 1])
+                return True
+            elif target < midValue:
+                rowId = left
+        if right == rowM:
+            # midM, midN = right // rowN, right % rowN
+            midValue = matrix[right - 1][columnN - 1]
+            if target == midValue:
+                rowId = right-1
+                print("found ..", (right-1, columnN - 1), matrix[right-1][columnN - 1])
+                return True
+            elif target < midValue:
+                rowId = right-1
+        
+        if rowId == None:
+            return False
+        nums = matrix[rowId]
+        
+        left = 0
+        right = columnN
+        # print("determine the Cell")
+        columnId = None
+        while(left + 1 < right):
+            mid = (left + right) // 2
+            
+            midValue = nums[mid]
+            if target == midValue:
+                # print("found ..", (rowId, mid), matrix[rowId][mid])
+                return True
+            elif target > midValue:
+                left = mid
+            else:
+                right = mid
+        print(left, right, rowId)
+        if left < columnN - 1:
+            # midM, midN = left // rowN, left % rowN
+            midValue = nums[left]
+            if target == midValue:
+                columnId = left
+        if right == columnN:
+            # midM, midN = right // rowN, right % rowN
+            midValue = nums[right - 1]
+            if target == midValue:
+                columnId = right
+        if columnId != None:
+            # print("found ..", (rowId, columnId), matrix[rowId][columnId])
+            return True
+        else:
+            return False
+
+        
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if matrix == None:
+            return False
+        rowM = len(matrix)
+        columnN = None
+        if rowM > 0:
+            columnN = len(matrix[0])
+        else:
+            return False
+        left = 0
+        right = rowM * columnN
+        # print("starting", right, rowM, columnN)
+        while(left + 1 < right):
+            mid = (left + right) // 2
+            
+            #midValue = self.getValue(matrix, mid, columnN)
             # print("mid,", mid, midValue)
-            midM, midN = mid // rowN, mid % rowN
+            midM, midN = mid // columnN, mid % columnN
             midValue = matrix[midM][midN]
             if target == midValue:
                 return True
@@ -25,20 +106,20 @@ class Solution:
             else:
                 right = mid
         print(left, right)
-        if left < (rowM * rowN):
-            midM, midN = left // rowN, left % rowN
+        if left < (rowM * columnN):
+            midM, midN = left // columnN, left % columnN
             midValue = matrix[midM][midN]
             if target == midValue:
                   return True
-        if right < (rowM * rowN):
-            midM, midN = right // rowN, right % rowN
+        if right < (rowM * columnN):
+            midM, midN = right // columnN, right % columnN
             midValue = matrix[midM][midN]
             if target == midValue:
                   return True
         return False
     
-    def getValue(self, matrix, mid, rowN):
-        midM, midN = mid // rowN, mid % rowN
+    def getValue(self, matrix, mid, columnN):
+        midM, midN = mid // columnN, mid % columnN
         # print(mid, midM, midN)
         midValue = matrix[midM][midN]
         return midValue
